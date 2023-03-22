29a8b626040a80a1edf0c4ea8c96284d8cb67c08
DanielleSmith0 <Danielahsmith@icloud.com>
2023-03-22 14:55:02 -0600

    changed port to 4000

--- server.js
+++ server.js
@@ -17,15 +17,17 @@ app.get('/css', (req, res) => {
 })
 
 //now go to html and reconnect them properly.
+//not stylesheetcss
 
 app.get('/js', (req, res) => {
     res.sendFile(__dirname + '/public/main.js')
 })
 
 //Same with html and js.
+//not main.js
 
 app.listen(5150, () => {
-    console.log("app is up on 5150")
+    console.log("app is up on 4000")
 });
 
 //make sure your browswer is a local host instead of a live server - url should say http://localhost:5150/
